8a7d337ca4e49665eef7f44e6f091fcea9bd7163
KAMichels <example@mail.com>
2024-09-05 13:49:40 +0300

    add my first app

---
+++ Week1FirstApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string myName = "Kirsten";
+string myLastName = "Michels";
+//Console.WriteLine("Hello" + " " + myName);
+
+Console.WriteLine($"Hello, {myName} {myLastName}!");
